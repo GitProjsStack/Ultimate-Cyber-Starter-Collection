9c15295745c5f7d3bcc808f500df9b5558bedd1d
GitProjsStack <bbeeyyoonnddai@gmail.com>
2025-07-09 23:26:18 -0400

    npm run build & npm run export will now work

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',  // enable static HTML export
 };
 
 export default nextConfig;
